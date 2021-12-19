028d56a5783a4cbe0222e94e6ef8c4ee19093346
saralimbu2017 <iamsebi7@gmail.com>
2021-12-20 10:49:21 +1100

    Implementing RoleMiddleware in route

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/admin/user/roles',['middleware'=>'role',function(){
+    return "Middleware role";
+}]);
